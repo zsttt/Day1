4492eb70d75f7cdd6133308bdb20c12b5bd9e882
zsttt <zstap17@gmail.com>
2020-12-28 18:46:43 -0500

    2 inital commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,28 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Animals from '../public/animals.json'
+import Animal from './src/Animal'
+
 
 export default function Home() {
+
+  const animalMaker = p => <Animal type = {p.type} state = {p.state} img = {p.img} adoption_fee = {p.adoption_fee}/>
+  const names = Animals.map(animalMaker)
+
+  const chillAnimalsOnly = p => p.state = "chill"
+  const chillAnimals = Animals.filter(chillAnimalsOnly)
+  
+
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <html>
+      <div>
+        <h1>Animal Summary</h1>
+      </div>
+  
+      <div>
+        {chillAnimals.map(animalMaker)}
+      </div>
+    </html>
   )
 }
--- pages/src/Animal.js
+++ pages/src/Animal.js
@@ -1,4 +1,15 @@
-const Animal = 
+import Image from "next/Image"
+
+const Animal = props => {
+    return (
+        <div>
+            <strong>{props.type}</strong>
+            <span>{props.state}</span>    
+            <span>{props.adoption_fee}</span>
+            <Image width={100} height={100} src={"/"+props.img}></Image>
+        </div>
+    )
+} 
 
 
 export default Animal
