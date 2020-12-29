8b30493253434ea38b2ce8b53ea0918522a17cb3
zsttt <zstap17@gmail.com>
2020-12-29 10:15:54 -0500

    third commit

--- pages/index.js
+++ pages/index.js
@@ -9,20 +9,33 @@ export default function Home() {
   const animalMaker = p => <Animal type = {p.type} state = {p.state} img = {p.img} adoption_fee = {p.adoption_fee}/>
   const names = Animals.map(animalMaker)
 
-  const chillAnimalsOnly = p => p.state = "chill"
-  const chillAnimals = Animals.filter(chillAnimalsOnly)
+  const stateAnimalsOnly = p => p.state == "sad"
+  const stateAnimals = Animals.filter(stateAnimalsOnly)
+  const stateAnimalsFormat = stateAnimals.map(animalMaker)
+  
+  const sum = (x,y) => x+y.adoption_fee
+  const statePrice = stateAnimals.reduce(sum,0)
+
   
 
 
   return (
     <html>
       <div>
-        <h1>Animal Summary</h1>
+        <h1 class ="text-center text-3xl font-extrabold bg-green-100 p-6">Animal Assignment</h1>
+        <p class ="text-center text-2xl p-2">
+          I filtered out all of the animals that aren't sad
+        </p>
       </div>
   
-      <div>
-        {chillAnimals.map(animalMaker)}
+      <div class = "text-center">
+        {stateAnimalsFormat}
       </div>
+
+      <div>
+        <h1 class ="text-center text-3xl font-extrabold bg-green-100 p-6">Cost to adopt them all: {statePrice}</h1>
+      </div>
+
     </html>
   )
 }
--- pages/src/Animal.js
+++ pages/src/Animal.js
@@ -1,13 +1,23 @@
 import Image from "next/Image"
 
+
 const Animal = props => {
     return (
-        <div>
-            <strong>{props.type}</strong>
-            <span>{props.state}</span>    
-            <span>{props.adoption_fee}</span>
-            <Image width={100} height={100} src={"/"+props.img}></Image>
+
+        <div class = "inline-block text-center content-center border-2 border-white p-7 bg-yellow-100 block">
+           
+            <span class ="inline-block">
+                <Image width ={166.8} height={166.8} src={"/"+props.img}></Image>
+            </span>
+
+            <div class = "text-lg font-semibold">
+                <div>Animal: {props.type}</div>
+                <div>State: {props.state}</div>    
+                <div>Fee to adopt: {props.adoption_fee}</div>
+            </div>
+
         </div>
+
     )
 } 
 
